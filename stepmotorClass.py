8f554f37cb8228a9faf04b07939717454d68bffc
r91090701 <r91090701@gmail.com>
2020-11-25 23:00:38 +0800

    v1 DONE

--- stepmotorClass.py
+++ stepmotorClass.py
@@ -14,7 +14,7 @@ class stepmotor():
     self.b=b
     self.abar=abar
     self.bbar=bbar
-  def run(self,theta,speed):
+  def run(self,theta,speed,ZeroFlag):
         if theta<0:
       theta+=360
     step=round((theta/1.8),1)
@@ -42,33 +42,8 @@ class stepmotor():
         GPIO.output(self.bbar,0)
       self.Now+=1
       time.sleep(speed)
-    if theta!=0:
+    if theta!=0 and ZeroFlag!=True:
       time.sleep(4)
-      self.runtozero(360-theta,speed)
-  def runtozero(self,theta,speed):
-    theta=round((theta/1.8),1)
-    for r in range(int(theta)):
+      self.run(360-theta,speed,True)
 
-      if self.Now%4==0:
-        GPIO.output(self.a,0)
-        GPIO.output(self.b,1)
-        GPIO.output(self.abar,1)
-        GPIO.output(self.bbar,0)  
-      if self.Now%4==1:
-        GPIO.output(self.a,0)
-        GPIO.output(self.b,1)
-        GPIO.output(self.abar,0)
-        GPIO.output(self.bbar,1)
-      if self.Now%4==2:
-        GPIO.output(self.a,1)
-        GPIO.output(self.b,0)
-        GPIO.output(self.abar,0)
-        GPIO.output(self.bbar,1)
-      if self.Now%4==3:
-        GPIO.output(self.a,1)
-        GPIO.output(self.b,0)
-        GPIO.output(self.abar,1)
-        GPIO.output(self.bbar,0)
-      self.Now+=1
-      time.sleep(speed)
 m=stepmotor(6,13,19,26)
