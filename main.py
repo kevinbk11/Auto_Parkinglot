4e6a0bbc95f3a3322c53b8351db30e428740f566
r91090701 <r91090701@gmail.com>
2020-11-27 18:51:54 +0800

    V2 is done

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import threading
 import time
-'''import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import cv2 as cv
 import find
 import CutAndNet
 import os
 import stepmotorClass
 import torch
-import light'''
-#DataRoot=r"/home/pi/Desktop/work2/car-work/DataBase.pt"
+import SevenSegmentsClass
+DataRoot=r"/home/pi/Desktop/work2/car-work/DataBase.pt"
 CarInputRoot=r"C:\Users\User\Desktop\c++\Produce\data.txt"
-'''
+
 GPIO.setwarnings(0)
 
 m=stepmotorClass.m
 
 fp=open(DataRoot,"r")
-Seven=light.Segments
+Seven=SevenSegmentsClass.Segments
 x=fp.readline().split()
 r=0
 for h in x:
@@ -44,7 +44,7 @@ fMotor.start(0)
 bMotor.start(0)
 time.sleep(2)
 global delay
-delay=0'''
+delay=0
   
 def BackDoor():
     while True:
@@ -61,16 +61,10 @@ def BackDoor():
             bMotor.ChangeDutyCycle(write(90))
             time.sleep(5)
             bMotor.ChangeDutyCycle(write(0))
-'''
-todo
-在最一開始的時候讓他正轉一度在負轉一度 此時狀態在第一種 所以這時候內部變數要設定為1 也就是第二種
-內部變數是用來判斷該走哪一步的
-所以假設現在為0 代表要走第一種狀態 所以現在是在第四種狀態 要反轉就要走第三種狀態 也就是0-2+4
-假設現在為a 要反轉之前就要先把a-2在+4(避免為負數) 迴圈內部要-1 
-迴圈執行到一半的時候且還沒運轉時 如果內部變數等於-1 那就要把它+4'''
 
-'''cnn=CutAndNet.CNN()
-cnn.load_state_dict(torch.load("car-work/reallynet.pt"))'''
+
+cnn=CutAndNet.CNN()
+cnn.load_state_dict(torch.load("car-work/reallynet.pt"))
 def FrontDoor():
     global delay
     while True:
@@ -106,7 +100,7 @@ def FrontDoor():
                         time.sleep(5)
                         fMotor.ChangeDutyCycle(write(0))
                         time.sleep(3)
-                        m.run(w*45,0.025)
+                        m.run(w*45,0.025,True)
                         x[w]=ans
                         break
                 Seven.ChangeState(x.count("None"))
@@ -120,36 +114,38 @@ def FrontDoor():
                 cv.destroyAllWindows()
                 delay=0
             delay=0
-global CarList
-def CheckQueue():
-    while True:
-        fp=open(CarInputRoot,"")
-        fp=open(CarInputRoot,"r")
-        CarList=fp.readline().split()
-        fp.close()
+
         
     
-'''Front=threading.Thread(target=FrontDoor)
+Front=threading.Thread(target=FrontDoor)
 Back=threading.Thread(target=BackDoor)
 Front.start()
 Back.start()
-GPIO.setup(btm,GPIO.IN)'''
+global CarList
+def CheckQueue():
+    global CarList
+    while True:
+        time.sleep(0.1)
+        fp=open(CarInputRoot,"r")
+        CarList=fp.readline().split()
+        fp.close()
+Check=threading.Thread(target=CheckQueue)
+Check.start()
 while True:
     time.sleep(2)
-    fp=open(CarInputRoot,"r")
-    CarList=fp.readline().split()
-    fp.close()
     while len(CarList)>0:
+        time.sleep(3)
+        print(CarList)
         delay=999999
         
-        '''w=open(DataRoot,"r")
+        w=open(DataRoot,"r")
         n=w.readline().split()
         w.close()
 
         for f in range(8):
             if n[f]==CarList[0]:
                 n[f]="None"
-                m.run(180-45*f,0.025)
+                m.run(180-45*f,0.025,True)
                 count=n.count("None")
                 Seven.ChangeState(count)
                 break
@@ -163,7 +159,7 @@ while True:
         fff=open(DataRoot,"a")
         for a in n:
             fff.write(a+" ")
-        fff.close()'''
+        fff.close()
         f=open(CarInputRoot,"w")
         f.write("")
         f.close()
@@ -178,47 +174,4 @@ while True:
                 f.write(oldCarList[x]+" ")
             CarList.append(oldCarList[x])
         f.close()
-
-
     delay=0
-
-
-
-
-
-
-
-
-
-
-
-'''x = input()
-w=open(DataRoot,"r")
-n=w.readline().split()
-for f in range(8):
-    if n[f]==x:
-        n[f]="None"
-w.close()
-ww=open(DataRoot,"w")
-ww.write("")
-ww.close()
-www=open(DataRoot,"a")
-for h in n:
-    www.write(h+" ")
-www.close()
-print("??")
-ggg=open(DataRoot,"r")
-gg=ggg.readline().split()
-gg[6]="KOPWERKOP"
-ggg.close()
-ff=open(DataRoot,"w")
-ff.write("")
-ff.close()
-fff=open(DataRoot,"a")
-
-for a in gg:
-    fff.write(a+" ")
-FrontDoorMotor.open()
-time.sleep(10)
-FrontDoorMotor.close()'''
-#Front.start()
