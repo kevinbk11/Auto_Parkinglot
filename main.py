ef52188f1a3a609db73cecf54fc66134b4fef290
r91090701 <r91090701@gmail.com>
2020-11-27 18:29:41 +0800

    testing

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import threading
 import time
-import RPi.GPIO as GPIO
+'''import RPi.GPIO as GPIO
 import cv2 as cv
 import find
 import CutAndNet
 import os
 import stepmotorClass
 import torch
-import light
-DataRoot=r"/home/pi/Desktop/work2/car-work/DataBase.pt"
-CarInputRoot=r"/home/pi/Desktop/work2/car-work/data.txt"
+import light'''
+#DataRoot=r"/home/pi/Desktop/work2/car-work/DataBase.pt"
+CarInputRoot=r"C:\Users\User\Desktop\c++\Produce\data.txt"
+'''
 GPIO.setwarnings(0)
 
 m=stepmotorClass.m
@@ -43,7 +44,7 @@ fMotor.start(0)
 bMotor.start(0)
 time.sleep(2)
 global delay
-delay=0
+delay=0'''
   
 def BackDoor():
     while True:
@@ -61,21 +62,15 @@ def BackDoor():
             time.sleep(5)
             bMotor.ChangeDutyCycle(write(0))
 '''
-
 todo
-
 在最一開始的時候讓他正轉一度在負轉一度 此時狀態在第一種 所以這時候內部變數要設定為1 也就是第二種
-
 內部變數是用來判斷該走哪一步的
-
 所以假設現在為0 代表要走第一種狀態 所以現在是在第四種狀態 要反轉就要走第三種狀態 也就是0-2+4
-
 假設現在為a 要反轉之前就要先把a-2在+4(避免為負數) 迴圈內部要-1 
-
 迴圈執行到一半的時候且還沒運轉時 如果內部變數等於-1 那就要把它+4'''
 
-cnn=CutAndNet.CNN()
-cnn.load_state_dict(torch.load("car-work/reallynet.pt"))
+'''cnn=CutAndNet.CNN()
+cnn.load_state_dict(torch.load("car-work/reallynet.pt"))'''
 def FrontDoor():
     global delay
     while True:
@@ -125,24 +120,29 @@ def FrontDoor():
                 cv.destroyAllWindows()
                 delay=0
             delay=0
-        
+global CarList
+def CheckQueue():
+    while True:
+        fp=open(CarInputRoot,"")
+        fp=open(CarInputRoot,"r")
+        CarList=fp.readline().split()
+        fp.close()
         
     
-Front=threading.Thread(target=FrontDoor)
+'''Front=threading.Thread(target=FrontDoor)
 Back=threading.Thread(target=BackDoor)
 Front.start()
 Back.start()
-GPIO.setup(btm,GPIO.IN)
+GPIO.setup(btm,GPIO.IN)'''
 while True:
     time.sleep(2)
     fp=open(CarInputRoot,"r")
     CarList=fp.readline().split()
     fp.close()
     while len(CarList)>0:
-        
         delay=999999
         
-        w=open(DataRoot,"r")
+        '''w=open(DataRoot,"r")
         n=w.readline().split()
         w.close()
 
@@ -163,8 +163,7 @@ while True:
         fff=open(DataRoot,"a")
         for a in n:
             fff.write(a+" ")
-        fff.close()
-
+        fff.close()'''
         f=open(CarInputRoot,"w")
         f.write("")
         f.close()
@@ -173,13 +172,15 @@ while True:
         CarList.clear()
         f=open(CarInputRoot,"a")
         for x in range(1,len(oldCarList)):
-            f.write(oldCarList[x]+" ")
+            if x==len(oldCarList)-1:
+                f.write(oldCarList[x])
+            else:
+                f.write(oldCarList[x]+" ")
             CarList.append(oldCarList[x])
         f.close()
 
 
     delay=0
-
 
 
 
@@ -221,9 +222,3 @@ FrontDoorMotor.open()
 time.sleep(10)
 FrontDoorMotor.close()'''
 #Front.start()
-
-
-
-
-            
-    
